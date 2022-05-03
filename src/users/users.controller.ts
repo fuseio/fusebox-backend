3d5ded314154a791595c6ce5d8af31a1514e68de
Maor Stamati <maorstamati@gmail.com>
2022-05-03 17:23:04 +0300

    Remove unused import

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.decorator';

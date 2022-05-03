e231ead2cd52e9b8cf664027ce3e0ad103ff9415
Maor Stamati <maorstamati@gmail.com>
2022-05-03 14:18:51 +0300

    Initial commit - project boilerplate structure

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({
+    disableErrorMessages: true,
+  }));
+  await app.listen(process.env.PORT);
+}
+bootstrap();

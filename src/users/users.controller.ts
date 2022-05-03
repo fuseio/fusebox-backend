f83a5e3577052b39842c255b8cbe6fb13bfb230d
Maor Stamati <maorstamati@gmail.com>
2022-05-03 17:14:58 +0300

    Create JwtAuthGuard class to avoid magic strings in code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { User } from './user.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Get()
   findOne(@User('sub') id: string) {
     return this.usersService.findOne(id);

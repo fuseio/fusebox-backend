2073483b49190e32848fd2e67fb5119201ba154b
Maor Stamati <maorstamati@gmail.com>
2022-05-22 23:24:23 +0300

    Changed 'testApp' to 'chargeApp'

--- src/studio-legacy-jwt/studio-legacy-jwt.service.ts
+++ src/studio-legacy-jwt/studio-legacy-jwt.service.ts
@@ -8,39 +8,39 @@ dotenv.config();
 
 @Injectable()
 export class StudioLegacyJwtService {
-  constructor(private httpService: HttpService) {}
+    constructor(private httpService: HttpService) { }
 
-  async create(
-    appName = 'testApp',
-  ): Promise<Observable<AxiosResponse<string>>> {
-    const requestConfig: AxiosRequestConfig = {
-      headers: {
-        'Content-Type': 'application/json', // afaik this one is not needed
-        Authorization: `Bearer ${process.env.FUSE_STUDIO_ADMIN_JWT}`,
-      },
-    };
+    async create(
+        appName = 'chargeApp',
+    ): Promise<Observable<AxiosResponse<string>>> {
+        const requestConfig: AxiosRequestConfig = {
+            headers: {
+                'Content-Type': 'application/json', // afaik this one is not needed
+                Authorization: `Bearer ${process.env.FUSE_STUDIO_ADMIN_JWT}`,
+            },
+        };
 
-    const requestBody = {
-      role: 'communityAdmin',
-      bridgeType: 'home',
-      description: 'charge',
-      appName: appName,
-    };
+        const requestBody = {
+            role: 'communityAdmin',
+            bridgeType: 'home',
+            description: 'charge',
+            appName: appName,
+        };
 
-    const responseData = await lastValueFrom(
-      this.httpService
-        .post(
-          `${process.env.FUSE_STUDIO_ACCOUNTS_API_URL}`,
-          requestBody,
-          requestConfig,
-        )
-        .pipe(
-          map((response) => {
-            return response.data;
-          }),
-        ),
-    );
+        const responseData = await lastValueFrom(
+            this.httpService
+                .post(
+                    `${process.env.FUSE_STUDIO_ACCOUNTS_API_URL}`,
+                    requestBody,
+                    requestConfig,
+                )
+                .pipe(
+                    map((response) => {
+                        return response.data;
+                    }),
+                ),
+        );
 
-    return responseData;
-  }
+        return responseData;
+    }
 }

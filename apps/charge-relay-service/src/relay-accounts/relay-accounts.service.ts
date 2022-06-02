7427346336b1db6fadc25851f59a964013f603c3
Lior Agnin <lior.agnin@gmail.com>
2022-06-02 12:01:59 +0300

    minor fix

--- apps/charge-relay-service/src/relay-accounts/relay-accounts.service.ts
+++ apps/charge-relay-service/src/relay-accounts/relay-accounts.service.ts
@@ -1,5 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { RpcException } from '@nestjs/microservices';
 import { Model } from 'mongoose';
 import { relayAccountModelString } from '@app/relay-service/relay-accounts/relay-accounts.constants';
 import { RelayAccount } from '@app/relay-service/relay-accounts/interfaces/api-keys.interface ';
@@ -12,34 +11,6 @@ export class RelayAccountsService {
   ) {}
 
   async createAccount(projectId: string) {
-    const account = await this.relayAccountModel.findOne({ projectId });
-
-    if (account) {
-      throw new RpcException('Account already assigned to this project Id');
-    }
-
-    return {
-      account,
-    };
-
-    // const secretKey = `sk_${await this.generateRandomToken()}`;
-    // const saltRounds = await bcrypt.genSalt();
-    // const secretHash = await bcrypt.hash(secretKey, saltRounds);
-
-    // const result = await this.apiKeyModel.findOneAndUpdate(
-    //   { projectId: projectId },
-    //   {
-    //     secretHash: secretHash,
-    //   },
-    //   { upsert: true, new: true },
-    // );
-
-    // if (result) {
-    //   return {
-    //     secretKey: secretKey,
-    //   };
-    // }
-
-    // throw new RpcException('Internal Server Error');
+    console.log('Create account for ' + projectId);
   }
 }

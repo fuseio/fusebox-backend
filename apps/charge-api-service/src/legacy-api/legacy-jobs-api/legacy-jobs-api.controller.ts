1a5bcd9a7beb2a9f3b203894910db2e7055190e8
Maor Stamati <maorstamati@gmail.com>
2022-06-13 13:32:23 +0300

    - Add guards to where it was missing

--- apps/charge-api-service/src/legacy-api/legacy-jobs-api/legacy-jobs-api.controller.ts
+++ apps/charge-api-service/src/legacy-api/legacy-jobs-api/legacy-jobs-api.controller.ts
@@ -1,6 +1,6 @@
 import { IsValidApiKeysGuard } from '@app/api-service/api-keys/guards/is-valid-api-keys.guard'
 import { Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common'
-import { LegacyApiInterceptor } from '../legacy-api.interceptor'
+import { LegacyApiInterceptor } from '@app/api-service/legacy-api/legacy-api.interceptor'
 
 @UseGuards(IsValidApiKeysGuard)
 @UseInterceptors(LegacyApiInterceptor)
--- apps/charge-api-service/src/legacy-api/legacy-studio-api/legacy-studio-api.controller.ts
+++ apps/charge-api-service/src/legacy-api/legacy-studio-api/legacy-studio-api.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, UseInterceptors } from '@nestjs/common'
-import { LegacyApiInterceptor } from '../legacy-api.interceptor'
+import { IsValidApiKeysGuard } from '@app/api-service/api-keys/guards/is-valid-api-keys.guard'
+import { Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common'
+import { LegacyApiInterceptor } from '@app/api-service/legacy-api/legacy-api.interceptor'
 
+@UseGuards(IsValidApiKeysGuard)
 @UseInterceptors(LegacyApiInterceptor)
 @Controller({ path: 'v0/studio/*' })
 export class LegacyStudioApiController {

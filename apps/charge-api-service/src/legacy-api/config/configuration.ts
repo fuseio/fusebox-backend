3ed691b628e3404c09f7589984f0b8e9618aafde
Maor Stamati <maorstamati@gmail.com>
2022-06-20 15:18:23 +0300

    Necessary name changes in other places in the code base

--- apps/charge-api-service/src/legacy-api/config/configuration.ts
+++ apps/charge-api-service/src/legacy-api/config/configuration.ts
@@ -1,18 +1,18 @@
 export default () => ({
-  LegacyStudioApiController: {
-    baseUrl: `${process.env.LEGACY_FUSE_STUDIO_API_URL}/api/v2/admin`,
+  LegacyAdminApiController: {
+    baseUrl: `${process.env.LEGACY_FUSE_ADMIN_API_URL}/api/v2/admin`,
     replaceHeaders: true
   },
   LegacyJobsApiController: {
-    baseUrl: `${process.env.LEGACY_FUSE_STUDIO_API_URL}/api/v2/jobs`,
+    baseUrl: `${process.env.LEGACY_FUSE_ADMIN_API_URL}/api/v2/jobs`,
     replaceHeaders: true
   },
   LegacyWalletApiController: {
     baseUrl: `${process.env.LEGACY_FUSE_WALLET_API_URL}/api/v1`,
     replaceHeaders: false
   },
-  LegacyFuseswapApiController: {
-    baseUrl: `${process.env.LEGACY_FUSE_SWAP_API_URL}/api/v1`,
+  LegacyTradeApiController: {
+    baseUrl: `${process.env.LEGACY_FUSE_TRADE_API_URL}/api/v1`,
     replaceHeaders: false
   }
 })

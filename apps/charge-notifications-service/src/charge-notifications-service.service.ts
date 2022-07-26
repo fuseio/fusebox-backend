b5bb03b4ddfbce253677ec3df86c5a6a825cd7fb
Maor Stamati <maorstamati@gmail.com>
2022-07-26 10:55:12 +0300

    Remove unused function

--- apps/charge-notifications-service/src/charge-notifications-service.service.ts
+++ apps/charge-notifications-service/src/charge-notifications-service.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ChargeNotificationsService {
-  getHello (): string {
-    return 'Hello World!'
-  }
-}
+export class ChargeNotificationsService {}

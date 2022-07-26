c54d725a1015bcffc59553dec5a5c5c542435f14
Maor Stamati <maorstamati@gmail.com>
2022-07-26 10:56:16 +0300

    Remove unused service

--- apps/charge-notifications-service/src/charge-notifications-service.module.ts
+++ apps/charge-notifications-service/src/charge-notifications-service.module.ts
@@ -1,6 +1,5 @@
 import { BroadcasterModule } from '@app/notifications-service/broadcaster/broadcaster.module'
 import { ChargeNotificationsServiceController } from '@app/notifications-service/charge-notifications-service.controller'
-import { ChargeNotificationsService } from '@app/notifications-service/charge-notifications-service.service'
 import { EventsScannerModule } from '@app/notifications-service/events-scanner/events-scanner.module'
 import { TransactionsScannerModule } from '@app/notifications-service/transactions-scanner/transactions-scanner.module'
 import { WebhooksModule } from '@app/notifications-service/webhooks/webhooks.module'
@@ -8,7 +7,6 @@ import { Module } from '@nestjs/common'
 
 @Module({
   imports: [WebhooksModule, EventsScannerModule, TransactionsScannerModule, BroadcasterModule],
-  controllers: [ChargeNotificationsServiceController],
-  providers: [ChargeNotificationsService]
+  controllers: [ChargeNotificationsServiceController]
 })
 export class ChargeNotificationsServiceModule {}

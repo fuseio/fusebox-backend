5547423536d6be2154441879a1b84b92e9fc0c0d
Lior Agnin <lior.agnin@gmail.com>
2022-08-25 13:14:44 +0300

    minor fixes

--- apps/charge-defi-service/src/common/config/configuration.ts
+++ apps/charge-defi-service/src/common/config/configuration.ts
@@ -1,12 +1,10 @@
 export default () => ({
   blockRewardAddress: '0x63D4efeD2e3dA070247bea3073BCaB896dFF6C9B',
   consensusAddress: '0x3014ca10b91cb3D0AD85fEf7A3Cb95BCAc9c0f79',
+  defaultValidator: '0x28C32719Fe055cb959404f9c783eF731f1150DCE',
   rpcConfig: {
     rpc: {
       url: process.env.RPC_URL || 'https://rpc.fuse.io'
-      // networkName: process.env.NETWORK_NAME,
-      // chainId: parseInt(process.env.CHAIN_ID),
-      // maxBlocksToProcess: parseInt(process.env.MAX_BLOCKS) || 10000
     }
   }
 })

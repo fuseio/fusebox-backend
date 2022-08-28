cc01f8208583bc74c7d06986123390d2bb721c29
Lior Agnin <lior.agnin@gmail.com>
2022-08-28 11:33:37 +0300

    minor

--- apps/charge-network-service/src/farm/farm.service.ts
+++ apps/charge-network-service/src/farm/farm.service.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import Web3ProviderService from '@app/common/services/web3-provider.service'
 import { ConfigService } from '@nestjs/config'
-import ConsensusABI from '../common/constants/abi/Consensus.json'
 import MasterChefABI from '../common/constants/abi/MasterChef.json'
-import BlockRewardABI from '../common/constants/abi/BlockReward.json'
-import { formatEther, parseUnits } from 'nestjs-ethers'
 import { WithdrawDto } from './dto/withdraw.dto'
 import { DepositDto } from './dto/deposit.dto'
-
-const BLOCKS_IN_YEAR = 6307200
 
 @Injectable()
 export class FarmService {
@@ -19,61 +14,6 @@ export class FarmService {
 
   get web3Provider () {
     return this.web3ProviderService.getProvider()
-  }
-
-  async getBlockRewardAmount () {
-    const blockRewardContract = new this.web3Provider.eth.Contract(BlockRewardABI as any, this.configService.get('blockRewardAddress'))
-    const rewardPerBlock = await blockRewardContract.methods.getBlockRewardAmount().call()
-
-    return rewardPerBlock
-  }
-
-  async validatorFee (validator: string) {
-    const consensusContract = new this.web3Provider.eth.Contract(ConsensusABI as any, this.configService.get('consensusAddress'))
-    const fee = await consensusContract.methods.validatorFee(validator).call()
-
-    return fee
-  }
-
-  async getEstimatedAPR () {
-    const rewardPerBlock = await this.getBlockRewardAmount()
-    const { totalStakeAmount } = await this.getTotalStakeAmount()
-    const numberOfValidators = (await this.getValidators()).validators.length
-    const amount = parseUnits('1')
-    const fee = formatEther((await this.validatorFee(this.configService.get('defaultValidator'))))
-
-    const rewardPerYourBlocks = this.calcRewardPerYourBlocks(
-      rewardPerBlock,
-      amount,
-      numberOfValidators,
-      totalStakeAmount,
-      parseUnits((1 - parseFloat(fee)).toString())
-    )
-
-    const average = rewardPerYourBlocks.div(numberOfValidators)
-    const rewardPerYear = average.mul(BLOCKS_IN_YEAR)
-    const estimatedAPR = rewardPerYear.div(amount)
-    return {
-      estimatedAPR: formatEther(estimatedAPR)
-    }
-  }
-
-  async getValidators () {
-    const consensusContract = new this.web3Provider.eth.Contract(ConsensusABI as any, this.configService.get('consensusAddress'))
-    const validators = await consensusContract.methods.getValidators().call()
-
-    return {
-      validators
-    }
-  }
-
-  async getTotalStakeAmount () {
-    const consensusContract = new this.web3Provider.eth.Contract(ConsensusABI as any, this.configService.get('consensusAddress'))
-    const totalStakeAmount = await consensusContract.methods.totalStakeAmount().call()
-
-    return {
-      totalStakeAmount
-    }
   }
 
   async withdraw (withdrawDto: WithdrawDto) {
@@ -117,13 +57,4 @@ export class FarmService {
       transactionObject
     }
   }
-
-  // async getDelegatedAmount (delegatedAmountDto: DelegatedAmountDto) {
-  //   const consensusContract = new this.web3Provider.eth.Contract(ConsensusABI as any, this.configService.get('consensusAddress'))
-  //   const delegatedAmount = await consensusContract.methods.delegatedAmount(delegatedAmountDto.delegatorAddress, delegatedAmountDto.validatorAddress).call()
-
-  //   return {
-  //     delegatedAmount
-  //   }
-  // }
 }

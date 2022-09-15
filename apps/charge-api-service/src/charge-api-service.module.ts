67bd13cf048a291c842d9071a83b2b0698ed491f
Nikolay <dnolnque@gmail.com>
2022-09-15 14:22:48 +0300

    path hotfix

--- apps/charge-api-service/src/charge-api-service.module.ts
+++ apps/charge-api-service/src/charge-api-service.module.ts
@@ -5,7 +5,7 @@ import { Module } from '@nestjs/common'
 import { ApiKeyModule } from '@app/api-service/api-keys/api-keys.module'
 import { NotificationsModule } from '@app/api-service/notifications/notifications.module'
 import { FarmAPIModule } from '@app/api-service/farm-api/farm-api.module'
-import { TransferApiModule } from '@app/api-service/network/transfer-api/transfer-api.module'
+import { TransferApiModule } from '@app/api-service/transfer-api/transfer-api.module'
 
 @Module({
   imports: [ApiKeyModule, LegacyApiModule, NotificationsModule, FarmAPIModule, TransferApiModule],

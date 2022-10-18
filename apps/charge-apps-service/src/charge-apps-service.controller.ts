6ef22fe0cbf4b64977ecf3cea088f176c63296b3
Maor Stamati <maorstamati@gmail.com>
2022-10-18 13:53:19 +0200

    Charge apps service configuration

--- apps/charge-apps-service/src/charge-apps-service.controller.ts
+++ apps/charge-apps-service/src/charge-apps-service.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { ChargeAppsServiceService } from './charge-apps-service.service';
 
 @Controller()
 export class ChargeAppsServiceController {
-  constructor(private readonly chargeAppsServiceService: ChargeAppsServiceService) {}
-
-  @Get()
-  getHello(): string {
-    return this.chargeAppsServiceService.getHello();
+  @Get('health')
+  healthCheck () {
+    return 'ok'
   }
 }
--- apps/charge-apps-service/src/charge-apps-service.module.ts
+++ apps/charge-apps-service/src/charge-apps-service.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ChargeAppsServiceController } from './charge-apps-service.controller';
-import { ChargeAppsServiceService } from './charge-apps-service.service';
+import { ChargeAppsServiceController } from '@app/apps-service/charge-apps-service.controller';
+import { ChargeAppsServiceService } from '@app/apps-service/charge-apps-service.service';
 
 @Module({
   imports: [],

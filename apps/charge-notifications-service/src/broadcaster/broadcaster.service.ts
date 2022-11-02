45fa6c6b5e70c6735730d7391e4c001573e6519b
Maor Stamati <maorstamati@gmail.com>
2022-11-02 16:38:34 +0200

    Added logs for sent webhook data

--- apps/charge-notifications-service/src/broadcaster/broadcaster.service.ts
+++ apps/charge-notifications-service/src/broadcaster/broadcaster.service.ts
@@ -69,6 +69,7 @@ export class BroadcasterService {
       )
       .pipe(
         map((axiosResponse: AxiosResponse) => {
+          this.logger.log(`Sent webhook to ${webhookUrl} with data: ${postBody}`)
           return axiosResponse.data
         })
       )

b3239b8adfbbd089e1d21e3a37f109e22ff22cbe
Maor Stamati <maorstamati@gmail.com>
2023-01-24 11:49:47 +0200

    Removed unused file

--- apps/charge-smart-accounts-service/src/common/config/rpc-config.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-  rpcConfig: {
-    rpc: {
-      url: process.env.RPC_URL || 'https://rpc.fuse.io',
-      networkName: process.env.NETWORK_NAME,
-      chainId: parseInt(process.env.CHAIN_ID)
-    }
-  }
-})

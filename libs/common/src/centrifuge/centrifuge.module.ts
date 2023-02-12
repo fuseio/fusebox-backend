f86d7da99b97fffeb6c5ccb4524229661428d4e1
Lior Agnin <lior.agnin@gmail.com>
2023-02-12 23:35:06 +0200

    minor fix

--- libs/common/src/centrifuge/centrifuge.module.ts
+++
@@ -1,8 +0,0 @@
-// import { Module } from '@nestjs/common'
-// import { CentrifugeProvider } from '@app/common/centrifuge/centrifuge.provider'
-
-// @Module({
-//   providers: [CentrifugeProvider],
-//   exports: [CentrifugeProvider]
-// })
-// export class CentrifugeModule {}

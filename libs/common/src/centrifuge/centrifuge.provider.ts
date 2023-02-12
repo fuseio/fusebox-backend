788bb4a23bbd0acb0e14f9576353d93092e83e4b
Lior Agnin <lior.agnin@gmail.com>
2023-02-12 23:36:14 +0200

    m

--- libs/common/src/centrifuge/centrifuge.provider.ts
+++ libs/common/src/centrifuge/centrifuge.provider.ts
@@ -5,7 +5,6 @@ import { Provider } from '@nestjs/common'
 export const CentrifugeProvider: Provider = {
   provide: Centrifuge,
   useFactory: (): Centrifuge => new Centrifuge(process.env.CENTRIFUGO_URI, {
-    debug: true,
     websocket: WebSocket,
     name: 'charge-backend',
     token: process.env.CENTRIFUGO_JWT

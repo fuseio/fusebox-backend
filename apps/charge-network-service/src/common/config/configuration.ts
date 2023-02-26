75ad1799c62201a9675d65f215e29c0c9443c74c
Lior Agnin <lior.agnin@gmail.com>
2023-02-26 14:58:36 +0200

    oops

--- apps/charge-network-service/src/common/config/configuration.ts
+++ apps/charge-network-service/src/common/config/configuration.ts
@@ -15,7 +15,7 @@ export default () => ({
       tokenSymbol: 'VOLT',
       tokenName: 'VoltToken',
       tokenLogoURI: 'https://raw.githubusercontent.com/voltfinance/token-logos/main/logos/0x34Ef2Cc892a88415e9f02b91BfA9c91fC0bE6bD4/logo.png',
-      unstakeTokenAddress: '0x97a6e78c9208c21afaDa67e7E61d7ad27688eFd1',
+      unStakeTokenAddress: '0x97a6e78c9208c21afaDa67e7E61d7ad27688eFd1',
       stakingProviderId: 'volt-bar'
     }
   ]

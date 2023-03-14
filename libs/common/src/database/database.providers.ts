99afb7da98c406f510d2b1c51915a05059266f00
Lior Agnin <lior.agnin@gmail.com>
2023-03-14 16:16:17 +0200

    deprecation warning fixed

--- libs/common/src/database/database.providers.ts
+++ libs/common/src/database/database.providers.ts
@@ -3,7 +3,9 @@ import mongoose from 'mongoose'
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(process.env.MONGO_URI)
+    useFactory: (): Promise<typeof mongoose> => {
+      mongoose.set('strictQuery', false)
+      return mongoose.connect(process.env.MONGO_URI)
+    }
   }
 ]

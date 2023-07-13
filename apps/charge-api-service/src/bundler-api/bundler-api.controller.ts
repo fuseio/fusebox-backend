75b8dbc86a23423b103eb726affefb40b7615dd1
Nikolay <dnolnque@gmail.com>
2023-07-13 11:12:03 +0300

    lint

--- apps/charge-api-service/src/bundler-api/bundler-api.controller.ts
+++ apps/charge-api-service/src/bundler-api/bundler-api.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common'
 import { BundlerApiInterceptor } from '@app/api-service/bundler-api/bundler-api.interceptor'
 import { IsPrdOrSbxKeyGuard } from '@app/api-service/api-keys/guards/is-production-or-sandbox-key.guard'
-import { GrpcMethod } from '@nestjs/microservices'
 
 @UseGuards(IsPrdOrSbxKeyGuard)
 @Controller({ path: 'v0/bundler' })

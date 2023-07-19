7a8a6ad447911e01bc291cf09202b6ca38e8b102
Nikolay <dnolnque@gmail.com>
2023-07-19 15:31:19 +0300

    minor fixes

--- apps/charge-accounts-service/src/projects/projects.service.ts
+++ apps/charge-accounts-service/src/projects/projects.service.ts
@@ -5,7 +5,7 @@ import { projectModelString } from '@app/accounts-service/projects/projects.cons
 import { UsersService } from '@app/accounts-service/users/users.service'
 import { apiService } from '@app/common/constants/microservices.constants'
 import { Inject, Injectable } from '@nestjs/common'
-import { ClientProxy } from '@nestjs/microservices'
+import { ClientProxy, RpcException } from '@nestjs/microservices'
 import { Model } from 'mongoose'
 import { callMSFunction } from '@app/common/utils/client-proxy'
 import base64url from 'base64url'

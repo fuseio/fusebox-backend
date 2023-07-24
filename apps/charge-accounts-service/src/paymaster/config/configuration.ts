c7e027914a91816239244fcd9868e1de459318df
Nikolay <dnolnque@gmail.com>
2023-07-24 10:54:01 +0300

    deleted test config

--- apps/charge-accounts-service/src/paymaster/config/configuration.ts
+++ apps/charge-accounts-service/src/paymaster/config/configuration.ts
@@ -11,18 +11,6 @@ export default () => ({
           process.env.PAYMASTER_SANDBOX_CONTRACT_ADDRESS_V_0_1_0,
         entrypointAddress: process.env.ENTRYPOINT_SANDBOX_CONTRACT_ADDRESS_V_0_1_0
       }
-    },
-    '0_2_0': {
-      production: {
-        paymasterContractAddress:
-          process.env.PAYMASTER_PRODUCTION_CONTRACT_ADDRESS_V_0_1_0,
-        entrypointAddress: process.env.ENTRYPOINT_PRODUCTION_CONTRACT_ADDRESS_V_0_1_0
-      },
-      sandbox: {
-        paymasterContractAddress:
-          process.env.PAYMASTER_SANDBOX_CONTRACT_ADDRESS_V_0_1_0,
-        entrypointAddress: process.env.ENTRYPOINT_SANDBOX_CONTRACT_ADDRESS_V_0_1_0
-      }
     }
   }
 })

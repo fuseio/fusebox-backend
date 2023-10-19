b8fee178343504dba4476e5d8a739e7fe49b150d
Samet Sahin <a.sahin.ual@gmail.com>
2023-10-19 17:56:33 +0300

    before authenticating the wallet, subscribe the wallet address into notifications

--- apps/charge-smart-wallets-service/src/smart-wallets/services/smart-wallets-aa.service.ts
+++ apps/charge-smart-wallets-service/src/smart-wallets/services/smart-wallets-aa.service.ts
@@ -3,6 +3,8 @@ import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { arrayify, computeAddress, hashMessage, recoverPublicKey } from 'nestjs-ethers'
 import { SmartWalletService } from '@app/smart-wallets-service/smart-wallets/interfaces/smart-wallets.interface'
+import { NotificationsService } from '@app/api-service/notifications/notifications.service'
+import { ConfigService } from '@nestjs/config'
 // import { ISmartWalletUser } from '@app/common/interfaces/smart-wallet.interface'
 // import CentrifugoAPIService from '@app/common/services/centrifugo.service'
 
@@ -11,16 +13,21 @@ export class SmartWalletsAAService implements SmartWalletService {
   private readonly logger = new Logger(SmartWalletsAAService.name)
 
   constructor (
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
+    private readonly notificationsService: NotificationsService,
+    private configService: ConfigService,
     // private readonly centrifugoAPIService: CentrifugoAPIService,
   ) { }
 
   async auth (smartWalletsAuthDto: SmartWalletsAuthDto) {
     try {
+      const smartWalletAddress = smartWalletsAuthDto.smartWalletAddress
+      await this.subscribeWalletToNotifications(smartWalletAddress)
+
       const publicKey = recoverPublicKey(arrayify(hashMessage(arrayify(smartWalletsAuthDto.hash))), smartWalletsAuthDto.signature)
       const recoveredAddress = computeAddress(publicKey)
 
-      if (recoveredAddress === smartWalletsAuthDto.ownerAddress && smartWalletsAuthDto.smartWalletAddress) {
+      if (recoveredAddress === smartWalletsAuthDto.ownerAddress && smartWalletAddress) {
         const jwt = this.jwtService.sign({
           sub: recoveredAddress,
           info: {
@@ -39,5 +46,12 @@ export class SmartWalletsAAService implements SmartWalletService {
     }
   }
 
+  private async subscribeWalletToNotifications (walletAddress: string) {
+    const webhookID = this.configService.get('CHARGE_WEBHOOK_ID')
+
+    return this.notificationsService.createAddresses({
+      webhookId: webhookID, addresses: [walletAddress]
+    })
+  }
   // async getHistoricalTxs (user: ISmartWalletUser) { }
 }

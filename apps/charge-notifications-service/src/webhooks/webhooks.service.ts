298dc4235b8e5dbeeca76c3e511fb07475b1f7e8
Samet Sahin <a.sahin.ual@gmail.com>
2023-11-04 16:47:31 +0300

    if the webhook event already exists, do not create it

--- apps/charge-notifications-service/src/webhooks/webhooks.service.ts
+++ apps/charge-notifications-service/src/webhooks/webhooks.service.ts
@@ -134,9 +134,30 @@ export class WebhooksService {
         !isEmpty(eventType) &&
         this.isRelevantEvent(eventData.tokenType, eventType)) {
         try {
+          const webhookEvent = await this.webhookEventModel.findOne({
+            webhook: webhookId,
+            'eventData.txHash': eventData.txHash
+          })
+
+          if (webhookEvent) {
+            this.logger.debug(
+              `Webhook event for tx ${eventData.txHash} already exists.`,
+              'Not creating another webhook event for this tx.'
+            )
+            continue
+          }
+
+          console.log(
+            `Creating a new webhook event for the tx ${eventData.txHash}`
+          )
+
           await this.webhookEventModel.create({
             webhook: webhookId, projectId, webhookUrl, eventData, direction, addressType
           })
+
+          console.log(
+            `Created a new webhook event for the tx ${eventData.txHash}`
+          )
         } catch (err) {
           this.logger.error(`Webhook event couldn't be added to the DB: ${err}`)
         }

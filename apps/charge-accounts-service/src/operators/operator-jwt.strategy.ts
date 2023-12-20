df223e64e4499aa9fc5e18660441f8a22b62d7a3
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2023-12-20 15:44:19 +0000

    Add configService in OperatorJwtStrategy

--- apps/charge-accounts-service/src/operators/operator-jwt.strategy.ts
+++ apps/charge-accounts-service/src/operators/operator-jwt.strategy.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { operatorJwtString } from '@app/accounts-service/operators/operators.constants';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class OperatorJwtStrategy extends PassportStrategy(Strategy, operatorJwtString) {
-  constructor() {
+  constructor (
+    private readonly configService: ConfigService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: `${process.env.SMART_WALLETS_JWT_SECRET}`,
+      secretOrKey: configService.get('SMART_WALLETS_JWT_SECRET'),
     });
   }
 

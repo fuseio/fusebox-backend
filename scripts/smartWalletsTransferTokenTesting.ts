085c16a7eae01759dbbca3c0d36cd9dbf5a059ad
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2024-01-01 14:43:08 +0500

    Add test for the legacy token sending flow and FuseLegacySDK

--- scripts/smartWalletsTransferTokenTesting.ts
+++ scripts/smartWalletsTransferTokenTesting.ts
@@ -1,24 +1,48 @@
-import { ethers } from 'ethers';
 import { FuseSDK } from '@fuseio/fusebox-web-sdk';
-import { parseEther } from 'ethers/lib/utils';
+import { ethers } from 'ethers';
+import { parseEther } from 'nestjs-ethers';
+import { FuseLegacySDK } from '@scripts/fuseLegacySDK';
 
-export const transferToken = async (
-  privateKey: string,
-  to: string,
-  eth: string = "0.001"
-) => {
-  try {
-    const credentials = new ethers.Wallet(privateKey as string);
-    const publicApiKey = process.env.PUBLIC_API_KEY as string;
-    const fuseSDK = await FuseSDK.init(publicApiKey, credentials, { withPaymaster: true });
+export class TransferToken {
+  private _privateKey: string;
+  private _to: string;
+  private _eth: string;
 
-    const value = parseEther(eth);
-    const data = Uint8Array.from([]);
-    const res = await fuseSDK.callContract(to, value, data);
-    await res?.wait();
-    
-    return res;
-  } catch (error) {
-    throw new Error(error.message);
+  constructor(privateKey: string, to: string, eth: string = "0.001") {
+    this._privateKey = privateKey;
+    this._to = to;
+    this._eth = eth;
   }
-};
+
+  accountAbstraction = async () => {
+    try {
+      const credentials = new ethers.Wallet(this._privateKey);
+      const publicApiKey = process.env.PUBLIC_API_KEY;
+      const fuseSDK = await FuseSDK.init(publicApiKey, credentials, { withPaymaster: true });
+
+      const value = parseEther(this._eth);
+      const data = Uint8Array.from([]);
+      const res = await fuseSDK.callContract(this._to, value, data);
+      await res?.wait();
+
+      return res;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  legacy = async () => {
+    try {
+      const credentials = new ethers.Wallet(this._privateKey);
+      const publicApiKey = process.env.PUBLIC_API_KEY;
+      const fuseLegacySDK = await new FuseLegacySDK().init(publicApiKey, credentials);
+
+      const value = parseEther(this._eth);
+      const res = await fuseLegacySDK.relay(this._to, value);
+
+      return res;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+}
--- tests/apps/smart-wallets/transferToken.spec.ts
+++ tests/apps/smart-wallets/transferToken.spec.ts
@@ -1,8 +1,8 @@
 import { Wallet } from "ethers";
-import { transferToken } from "@scripts/smartWalletsTransferTokenTesting";
+import { TransferToken } from "@scripts/smartWalletsTransferTokenTesting";
 
-// The timeout is set to 5 minutes. While Jest's default timeout is 5 seconds,
-// account abstraction transactions require additional time.
+// The timeout is set to 5 minutes. While Jest's default
+// timeout is 5 seconds, transaction require additional time.
 const timeout = 300000;
 
 describe('Transfer Token', () => {
@@ -24,15 +24,37 @@ describe('Transfer Token', () => {
     ]);
   });
 
-  test('should successfully transfer token from account 1 to account 2', async () => {
-    await expect(transferToken(privateKey1, address2)).resolves.toBeDefined();
-  }, timeout);
+  describe('Account Abstraction', () => {
+    test('should successfully transfer token from account 1 to account 2', async () => {
+      const transferToken = new TransferToken(privateKey1, address2);
+      await expect(transferToken.accountAbstraction()).resolves.toBeDefined();
+    }, timeout);
 
-  test('should successfully transfer token from account 2 to account 1', async () => {
-    await expect(transferToken(privateKey2, address1)).resolves.toBeDefined();
-  }, timeout);
+    test('should successfully transfer token from account 2 to account 1', async () => {
+      const transferToken = new TransferToken(privateKey2, address1);
+      await expect(transferToken.accountAbstraction()).resolves.toBeDefined();
+    }, timeout);
 
-  test('should fail to transfer token', async () => {
-    await expect(transferToken("invalidPrivateKey", "invalidAddress")).rejects.toThrow();
-  }, timeout);
+    test('should fail to transfer token', async () => {
+      const transferToken = new TransferToken("invalidPrivateKey", "invalidAddress");
+      await expect(transferToken.accountAbstraction()).rejects.toThrow();
+    }, timeout);
+  })
+
+  describe('Legacy', () => {
+    test('should successfully transfer token from account 1 to account 2', async () => {
+      const transferToken = new TransferToken(privateKey1, address2);
+      await expect(transferToken.legacy()).resolves.toBeDefined();
+    }, timeout);
+
+    test('should successfully transfer token from account 2 to account 1', async () => {
+      const transferToken = new TransferToken(privateKey2, address1);
+      await expect(transferToken.legacy()).resolves.toBeDefined();
+    }, timeout);
+
+    test('should fail to transfer token', async () => {
+      const transferToken = new TransferToken("invalidPrivateKey", "invalidAddress");
+      await expect(transferToken.legacy()).rejects.toThrow();
+    }, timeout);
+  })
 });

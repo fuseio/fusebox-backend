e7bd4cb08df0c02c378249f33c16b5129f73f361
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2024-01-02 13:13:30 +0000

    Removing Volt staking from the staking options

--- apps/charge-network-service/src/common/config/configuration.ts
+++ apps/charge-network-service/src/common/config/configuration.ts
@@ -18,14 +18,6 @@ export default () => ({
   nftGraphUrl: 'https://api.thegraph.com/subgraphs/name/fuseio/fuse-nft-v2',
   stakingOptions: [
     {
-      tokenAddress: '0x34Ef2Cc892a88415e9f02b91BfA9c91fC0bE6bD4',
-      tokenSymbol: 'VOLT',
-      tokenName: 'VoltToken',
-      tokenLogoURI: 'https://raw.githubusercontent.com/voltfinance/token-logos/main/logos/0x34Ef2Cc892a88415e9f02b91BfA9c91fC0bE6bD4/logo.png',
-      unStakeTokenAddress: '0x97a6e78c9208c21afaDa67e7E61d7ad27688eFd1',
-      stakingProviderId: 'volt-bar'
-    },
-    {
       tokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
       tokenSymbol: 'FUSE',
       tokenName: 'Fuse',

6ae564435575c9606185f2f3fbd587967a06a372
Lior Agnin <lior.agnin@gmail.com>
2024-01-30 16:09:00 +0200

    Add index

--- apps/charge-smart-wallets-service/src/data-layer/schemas/user-op.schema.ts
+++ apps/charge-smart-wallets-service/src/data-layer/schemas/user-op.schema.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose'
 export const UserOpSchema = new mongoose.Schema(
   {
     sender: { type: String, required: true }, // Address is a string of hexadecimal characters
-    apiKey: { type: String, required: true }, // Api key of project
+    apiKey: { type: String, required: true, index: true }, // Api key of project
     nonce: { type: String, required: true }, // Nonce is a string representation of a number that increases with each operation
     initCode: { type: String, required: true }, // InitCode is a buffer of bytes that contains the code to create new wallets
     callData: { type: String, required: true }, // CallData is a buffer of bytes that contains the data to execute the action
@@ -14,13 +14,13 @@ export const UserOpSchema = new mongoose.Schema(
     maxPriorityFeePerGas: { type: Number, required: true }, // MaxPriorityFeePerGas is a number that specifies the maximum priority fee per gas unit for the operation
     paymasterAndData: { type: String, required: false }, // PaymasterAndData is a buffer of bytes that contains the address and data of the paymaster who pays for the operation
     signature: { type: String, required: true }, // Signature is a buffer of bytes that contains the signature of the sender
-    userOpHash: { type: String, required: true, default: '0x' },
+    userOpHash: { type: String, required: true, default: '0x', index: true },
     txHash: { type: String, required: false, default: '0x' },
     blockNumber: { type: String, required: false, default: '' },
     walletFunction: { type: Object, required: false, default: {} },
     targetFunctions: { type: Array, of: Object, required: false },
     paymaster: { type: String, required: true, default: '0x' },
-    sponsorId: { type: String, required: true, default: '0' },
+    sponsorId: { type: String, required: true, default: '0', index: true },
     success: { type: Boolean, required: true, default: false },
     actualGasCost: { type: Number, required: true, default: 0 },
     actualGasUsed: { type: Number, required: true, default: 0 }

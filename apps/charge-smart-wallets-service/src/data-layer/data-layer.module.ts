bd2a2c14bcaf887a1ad31b964b13e5d68a602639
Lior Agnin <lior.agnin@gmail.com>
2024-02-04 10:56:01 +0200

    Downgrade centrifugo

--- apps/charge-smart-wallets-service/src/data-layer/data-layer.module.ts
+++ apps/charge-smart-wallets-service/src/data-layer/data-layer.module.ts
@@ -13,6 +13,7 @@ import CentrifugoAPIService from '@app/common/services/centrifugo.service'
 import { CentrifugeProvider } from '@app/common/centrifuge/centrifuge.provider'
 import { HttpModule } from '@nestjs/axios'
 import { SmartWalletsAAEventsService } from '@app/smart-wallets-service/smart-wallets/smart-wallets-aa-events.service'
+
 @Module({
   imports: [DatabaseModule,
     ConfigModule.forFeature(web3Config),
@@ -20,9 +21,13 @@ import { SmartWalletsAAEventsService } from '@app/smart-wallets-service/smart-wa
 
   ],
   controllers: [DataLayerController],
-  providers: [DataLayerService,
-    ...dataLayerProviders, UserOpFactory,
-    UserOpParser, TokenService, Web3ProviderService,
+  providers: [
+    DataLayerService,
+    ...dataLayerProviders,
+    UserOpFactory,
+    UserOpParser,
+    TokenService,
+    Web3ProviderService,
     SmartWalletsAAEventsService,
     CentrifugoAPIService,
     CentrifugeProvider

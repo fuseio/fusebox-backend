8cd1da5fa5182284bf2c61ce8bb9953683061812
Lior Agnin <lior.agnin@gmail.com>
2024-02-13 11:21:17 +0200

    Store all gas values in string

--- apps/charge-smart-wallets-service/src/data-layer/interfaces/user-op.interface.ts
+++ apps/charge-smart-wallets-service/src/data-layer/interfaces/user-op.interface.ts
@@ -6,11 +6,11 @@ export interface BaseUserOp {
   nonce: string; // Nonce is a string representation of a number that increases with each operation
   initCode: string; // InitCode is a buffer of bytes that contains the code to create new wallets
   callData: string; // CallData is a buffer of bytes that contains the data to execute the action
-  callGasLimit: number; // CallGasLimit is a number that specifies the maximum gas to use for the action
-  verificationGasLimit: number; // VerificationGasLimit is a number that specifies the maximum gas to use for verifying the operation
-  preVerificationGas: number; // PreVerificationGas is a number that specifies the gas to use before verifying the operation
-  maxFeePerGas: number; // MaxFeePerGas is a number that specifies the maximum fee per gas unit for the operation
-  maxPriorityFeePerGas: number; // MaxPriorityFeePerGas is a number that specifies the maximum priority fee per gas unit for the operation
+  callGasLimit: string; // CallGasLimit is a number that specifies the maximum gas to use for the action
+  verificationGasLimit: string; // VerificationGasLimit is a number that specifies the maximum gas to use for verifying the operation
+  preVerificationGas: string; // PreVerificationGas is a number that specifies the gas to use before verifying the operation
+  maxFeePerGas: string; // MaxFeePerGas is a number that specifies the maximum fee per gas unit for the operation
+  maxPriorityFeePerGas: string; // MaxPriorityFeePerGas is a number that specifies the maximum priority fee per gas unit for the operation
   paymasterAndData?: string; // PaymasterAndData is a buffer of bytes that contains the address and data of the paymaster who pays for the operation
   paymaster: string; // Paymaster is an address of hexadecimal characters
   sponsorId: string; // SponsorId of user used in paymaster contract
@@ -22,6 +22,6 @@ export interface UserOp extends BaseUserOp, Document {
   walletFunction: object;
   targetFunctions: Array<object>;
   success: boolean; // Success is a boolean value that indicates whether the operation was successful or not
-  actualGasCost: number; // ActualGasCost is a number that indicates how much gas was spent for the operation
-  actualGasUsed: number; // ActualGasUsed is a number that indicates how much gas was used for the operation
+  actualGasCost: string; // ActualGasCost is a number that indicates how much gas was spent for the operation
+  actualGasUsed: string; // ActualGasUsed is a number that indicates how much gas was used for the operation
 }

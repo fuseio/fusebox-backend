5f19ce9e7fb13158dde05cccfe5fd8ae4c315f91
Lior Agnin <lior.agnin@gmail.com>
2024-02-13 11:21:31 +0200

    m

--- apps/charge-smart-wallets-service/src/data-layer/schemas/user-op.schema.ts
+++ apps/charge-smart-wallets-service/src/data-layer/schemas/user-op.schema.ts
@@ -7,11 +7,11 @@ export const UserOpSchema = new mongoose.Schema(
     nonce: { type: String, required: true }, // Nonce is a string representation of a number that increases with each operation
     initCode: { type: String, required: true }, // InitCode is a buffer of bytes that contains the code to create new wallets
     callData: { type: String, required: true }, // CallData is a buffer of bytes that contains the data to execute the action
-    callGasLimit: { type: Number, required: true }, // CallGasLimit is a number that specifies the maximum gas to use for the action
-    verificationGasLimit: { type: Number, required: true }, // VerificationGasLimit is a number that specifies the maximum gas to use for verifying the operation
-    preVerificationGas: { type: Number, required: true }, // PreVerificationGas is a number that specifies the gas to use before verifying the operation
-    maxFeePerGas: { type: Number, required: true }, // MaxFeePerGas is a number that specifies the maximum fee per gas unit for the operation
-    maxPriorityFeePerGas: { type: Number, required: true }, // MaxPriorityFeePerGas is a number that specifies the maximum priority fee per gas unit for the operation
+    callGasLimit: { type: String, required: true }, // CallGasLimit is a number that specifies the maximum gas to use for the action
+    verificationGasLimit: { type: String, required: true }, // VerificationGasLimit is a number that specifies the maximum gas to use for verifying the operation
+    preVerificationGas: { type: String, required: true }, // PreVerificationGas is a number that specifies the gas to use before verifying the operation
+    maxFeePerGas: { type: String, required: true }, // MaxFeePerGas is a number that specifies the maximum fee per gas unit for the operation
+    maxPriorityFeePerGas: { type: String, required: true }, // MaxPriorityFeePerGas is a number that specifies the maximum priority fee per gas unit for the operation
     paymasterAndData: { type: String, required: false }, // PaymasterAndData is a buffer of bytes that contains the address and data of the paymaster who pays for the operation
     signature: { type: String, required: true }, // Signature is a buffer of bytes that contains the signature of the sender
     userOpHash: { type: String, required: true, default: '0x', index: true },

dc6772a040848190ca67075ba2fb80774d600025
Lior Agnin <lior.agnin@gmail.com>
2024-05-23 10:07:55 +0300

    Minor imporvements

--- apps/charge-network-service/src/consensus/consensus.service.ts
+++ apps/charge-network-service/src/consensus/consensus.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, Injectable, Logger, Inject } from '@nestjs/common'
+import {
+  HttpException,
+  Injectable,
+  Logger,
+  Inject
+} from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import {
   Contract,
@@ -20,16 +25,25 @@ import { DelegatedAmountsDto } from '@app/network-service/consensus/dto/delegate
 import { IValidator } from '@app/network-service/consensus/interfaces'
 import { logPerformance } from '@app/notifications-service/common/decorators/log-performance.decorator'
 
+interface CacheOptions<T> {
+  cacheKey: string;
+  volatility: 'high' | 'medium' | 'low';
+  fetchFunction: () => Promise<T>;
+  forceRefresh?: boolean;
+  refreshCondition?: (cachedValue: T) => boolean;
+}
+
 @Injectable()
 export class ConsensusService {
   private readonly logger = new Logger(ConsensusService.name)
+
   constructor (
-    private httpService: HttpService,
+    private readonly httpService: HttpService,
     @InjectEthersProvider('regular-node')
     private readonly provider: JsonRpcProvider,
     private readonly configService: ConfigService,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache
-  ) { }
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache
+  ) {}
 
   @Cron(CronExpression.EVERY_5_MINUTES)
   async handleValidatorsUpdate () {
@@ -45,23 +59,23 @@ export class ConsensusService {
     await this.handleValidatorsUpdate()
   }
 
-  get multiCallAddress () {
+  private get multiCallAddress (): string {
     return this.configService.getOrThrow('multiCallAddress')
   }
 
-  get botApi () {
+  private get botApi (): string {
     return this.configService.getOrThrow('botApi')
   }
 
-  get consensusAddress () {
+  private get consensusAddress (): string {
     return this.configService.getOrThrow('consensusAddress')
   }
 
-  get consensusInterface (): Interface {
+  private get consensusInterface (): Interface {
     return new Interface(ConsensusAbi)
   }
 
-  get multiCallContract () {
+  private get multiCallContract (): Contract {
     return new Contract(
       this.multiCallAddress,
       MultiCallAbi,
@@ -70,24 +84,19 @@ export class ConsensusService {
   }
 
   async withdraw (validator: string, amount: string) {
-    return this.createTransactionObject(
-      this.encodeFunctionData('withdraw', [validator, amount])
-    )
+    return this.createTransactionObject('withdraw', [validator, amount])
   }
 
   async delegate (validator: string) {
-    return this.createTransactionObject(
-      this.encodeFunctionData('delegate', [validator])
-    )
+    return this.createTransactionObject('delegate', [validator])
   }
 
   async getCachedValidatorsInfo () {
-    const cachedInfo = await this.cacheManager.get('validatorsInfo')
-    if (isEmpty(cachedInfo)) {
-      return this.handleValidatorsUpdate()
-    }
-
-    return cachedInfo
+    return this.getCacheOrFetch({
+      cacheKey: 'validatorsInfo',
+      volatility: 'medium',
+      fetchFunction: () => this.handleValidatorsUpdate()
+    })
   }
 
   @logPerformance('ConsensusService::GetValidators')
@@ -116,7 +125,7 @@ export class ConsensusService {
       totalSupply
     ] = results
 
-    const combinedValidators = validators.concat(jailedValidators)
+    const combinedValidators = [...validators, ...jailedValidators]
     const {
       totalDelegators,
       validatorsMetadata
@@ -151,19 +160,17 @@ export class ConsensusService {
     const validatorDatas = await this.fetchValidatorDatas(validators)
     const validatorsMap = await this.fetchValidatorsMap()
 
-    const validatorsMetadata: IValidator[] = await Promise.all(
-      validators.map(async (validator, index) => {
-        const metadata = validatorDatas[index]
-
-        return this.buildValidatorMetadata(
+    const validatorsMetadata = await Promise.all(
+      validators.map((validator, index) =>
+        this.buildValidatorMetadata(
           validator,
-          metadata,
+          validatorDatas[index],
           validatorsMap,
           pendingValidators,
           totalSupply,
           totalStakeAmount
         )
-      })
+      )
     )
 
     const totalDelegators = validatorsMetadata.reduce(
@@ -192,20 +199,20 @@ export class ConsensusService {
     validatorsMap: Record<string, any>,
     pendingValidators: string[]
   ): IValidator {
-    const validatorData = validatorsMap[validator.toLowerCase()]
+    const validatorData = validatorsMap[validator.toLowerCase()] || {}
 
     return {
       ...metadata,
       address: validator,
-      name: validatorData?.name || validator,
-      website: validatorData?.website,
-      image: validatorData?.image,
+      name: validatorData.name || validator,
+      website: validatorData.website,
+      image: validatorData.image,
       status: metadata.isJailed ? 'inactive' : 'active',
       isPending: pendingValidators.includes(validator.toLowerCase()),
-      description: validatorData?.description,
+      description: validatorData.description,
       stakeAmount: metadata.stakeAmount,
       fee: metadata.fee,
-      delegatorsLength: metadata?.delegatorsLength || '0',
+      delegatorsLength: metadata.delegatorsLength || '0',
       delegators: metadata.delegators,
       isJailed: metadata.isJailed
     }
@@ -312,179 +319,171 @@ export class ConsensusService {
 
   @logPerformance('ConsensusService::GetValidatorFee')
   private async getValidatorFee (validator: string) {
-    const cacheKey = `validatorFee-${validator}`
-    let fee = await this.cacheManager.get<string>(cacheKey)
-    if (isEmpty(fee)) {
-      const [validatorFee] = await this.aggregateCalls([
-        {
-          method: 'validatorFee',
-          params: [validator]
-        }
-      ])
-      fee = validatorFee
-      await this.cacheManager.set(cacheKey, fee, this.cacheDuration('medium'))
-    }
-
-    return fee
+    return this.getCacheOrFetch({
+      cacheKey: `validatorFee-${validator}`,
+      volatility: 'medium',
+      fetchFunction: async () => {
+        const [validatorFee] = await this.aggregateCalls([
+          {
+            method: 'validatorFee',
+            params: [validator]
+          }
+        ])
+        return validatorFee
+      },
+      refreshCondition: cachedValue => isUndefined(cachedValue)
+    })
   }
 
-  private async isJailed (validator: string) {
-    const cacheKey = `isJailed-${validator}`
-    let jailedStatus = await this.cacheManager.get<boolean>(cacheKey)
-    if (isUndefined(jailedStatus)) {
-      const [isJailed] = await this.aggregateCalls([
-        {
-          method: 'isJailed',
-          params: [validator]
-        }
-      ])
-      jailedStatus = isJailed
-      await this.cacheManager.set(cacheKey, jailedStatus, this.cacheDuration('medium'))
-    }
-
-    return jailedStatus
+  private async isJailed (validator: string): Promise<boolean> {
+    return this.getCacheOrFetch({
+      cacheKey: `isJailed-${validator}`,
+      volatility: 'medium',
+      fetchFunction: async () => {
+        const [isJailed] = await this.aggregateCalls([
+          {
+            method: 'isJailed',
+            params: [validator]
+          }
+        ])
+        return isJailed
+      },
+      refreshCondition: cachedValue => isUndefined(cachedValue)
+    })
   }
 
   @logPerformance('ConsensusService::ReadTotalStakeAmount')
   private async readTotalStakeAmount () {
-    const cacheKey = 'totalStakeAmount'
-    let totalStakeAmount = await this.cacheManager.get<string>(cacheKey)
-    if (isEmpty(totalStakeAmount)) {
-      const [totalStakeAmountResult] = await this.aggregateCalls([
-        {
-          method: 'totalStakeAmount',
-          params: []
-        }
-      ])
-      totalStakeAmount = totalStakeAmountResult
-      await this.cacheManager.set(cacheKey, totalStakeAmount, this.cacheDuration('high'))
-    }
-
-    return totalStakeAmount
+    return this.getCacheOrFetch({
+      cacheKey: 'totalStakeAmount',
+      volatility: 'high',
+      fetchFunction: async () => {
+        const [totalStakeAmountResult] = await this.aggregateCalls([
+          {
+            method: 'totalStakeAmount',
+            params: []
+          }
+        ])
+        return totalStakeAmountResult
+      }
+    })
   }
 
   @logPerformance('ConsensusService::ReadValidators')
   private async readValidators () {
-    const cacheKey = 'validators'
-    let validators = await this.cacheManager.get<string[]>(cacheKey)
-    if (isEmpty(validators)) {
-      const [validatorsResult] = await this.aggregateCalls([
-        {
-          method: 'getValidators',
-          params: []
-        }
-      ])
-      validators = validatorsResult
-      await this.cacheManager.set(cacheKey, validators, this.cacheDuration('medium'))
-    }
-
-    return validators
+    return this.getCacheOrFetch({
+      cacheKey: 'validators',
+      volatility: 'medium',
+      fetchFunction: async () => {
+        const [validatorsResult] = await this.aggregateCalls([
+          {
+            method: 'getValidators',
+            params: []
+          }
+        ])
+        return validatorsResult
+      }
+    })
   }
 
   @logPerformance('ConsensusService::ReadJailedValidators')
   private async readJailedValidators () {
-    const cacheKey = 'jailedValidators'
-    let jailedValidators = await this.cacheManager.get<string[]>(cacheKey)
-    if (isEmpty(jailedValidators)) {
-      const [jailedValidatorsResult] = await this.aggregateCalls([
-        {
-          method: 'jailedValidators',
-          params: []
-        }
-      ])
-      jailedValidators = jailedValidatorsResult
-      await this.cacheManager.set(cacheKey, jailedValidators, this.cacheDuration('medium'))
-    }
-
-    return jailedValidators
+    return this.getCacheOrFetch({
+      cacheKey: 'jailedValidators',
+      volatility: 'medium',
+      fetchFunction: async () => {
+        const [jailedValidatorsResult] = await this.aggregateCalls([
+          {
+            method: 'jailedValidators',
+            params: []
+          }
+        ])
+        return jailedValidatorsResult
+      }
+    })
   }
 
   @logPerformance('ConsensusService::ReadMaxStake')
   private async readMaxStake () {
-    const cacheKey = 'maxStake'
-    let maxStake = await this.cacheManager.get<string>(cacheKey)
-    if (isEmpty(maxStake)) {
-      const [maxStakeResult] = await this.aggregateCalls([
-        {
-          method: 'getMaxStake',
-          params: []
-        }
-      ])
-      maxStake = maxStakeResult
-      await this.cacheManager.set(cacheKey, maxStake, this.cacheDuration('low'))
-    }
-
-    return maxStake
+    return this.getCacheOrFetch({
+      cacheKey: 'maxStake',
+      volatility: 'low',
+      fetchFunction: async () => {
+        const [maxStakeResult] = await this.aggregateCalls([
+          {
+            method: 'getMaxStake',
+            params: []
+          }
+        ])
+        return maxStakeResult
+      }
+    })
   }
 
   @logPerformance('ConsensusService::ReadMinStake')
   private async readMinStake () {
-    const cacheKey = 'minStake'
-    let minStake = await this.cacheManager.get<string>(cacheKey)
-    if (isEmpty(minStake)) {
-      const [minStakeResult] = await this.aggregateCalls([
-        {
-          method: 'getMinStake',
-          params: []
-        }
-      ])
-      minStake = minStakeResult
-      await this.cacheManager.set(cacheKey, minStake, this.cacheDuration('low'))
-    }
-
-    return minStake
+    return this.getCacheOrFetch({
+      cacheKey: 'minStake',
+      volatility: 'low',
+      fetchFunction: async () => {
+        const [minStakeResult] = await this.aggregateCalls([
+          {
+            method: 'getMinStake',
+            params: []
+          }
+        ])
+        return minStakeResult
+      }
+    })
   }
 
   @logPerformance('ConsensusService::ReadPendingValidators')
   private async readPendingValidators () {
-    const cacheKey = 'pendingValidators'
-    let pendingValidators = await this.cacheManager.get<string[]>(cacheKey)
-    if (isEmpty(pendingValidators)) {
-      const [pendingValidatorsResult] = await this.aggregateCalls([
-        {
-          method: 'pendingValidators',
-          params: []
-        }
-      ])
-      pendingValidators = pendingValidatorsResult
-      await this.cacheManager.set(cacheKey, pendingValidators, this.cacheDuration('low'))
-    }
-
-    return pendingValidators
+    return this.getCacheOrFetch({
+      cacheKey: 'pendingValidators',
+      volatility: 'low',
+      fetchFunction: async () => {
+        const [pendingValidatorsResult] = await this.aggregateCalls([
+          {
+            method: 'pendingValidators',
+            params: []
+          }
+        ])
+        return pendingValidatorsResult
+      }
+    })
   }
 
   private async readDelegators (validator: string) {
-    const cacheKey = `delegators-${validator}`
-    let delegators = await this.cacheManager.get<string[]>(cacheKey)
-    if (isEmpty(delegators)) {
-      const [delegatorsResult] = await this.aggregateCalls([
-        {
-          method: 'delegators',
-          params: [validator]
-        }
-      ])
-      delegators = delegatorsResult
-      await this.cacheManager.set(cacheKey, delegators, this.cacheDuration('medium'))
-    }
-
-    return delegators
+    return this.getCacheOrFetch({
+      cacheKey: `delegators-${validator}`,
+      volatility: 'medium',
+      fetchFunction: async () => {
+        const [delegatorsResult] = await this.aggregateCalls([
+          {
+            method: 'delegators',
+            params: [validator]
+          }
+        ])
+        return delegatorsResult
+      }
+    })
   }
 
   private async readStakeAmount (validator: string) {
-    const cacheKey = `stakeAmount-${validator}`
-    let stakeAmount = await this.cacheManager.get<string>(cacheKey)
-    if (isEmpty(stakeAmount)) {
-      const [stakeAmountResult] = await this.aggregateCalls([
-        {
-          method: 'stakeAmount',
-          params: [validator]
-        }
-      ])
-      stakeAmount = stakeAmountResult
-      await this.cacheManager.set(cacheKey, stakeAmount, this.cacheDuration('medium'))
-    }
-
-    return stakeAmount
+    return this.getCacheOrFetch({
+      cacheKey: `stakeAmount-${validator}`,
+      volatility: 'medium',
+      fetchFunction: async () => {
+        const [stakeAmountResult] = await this.aggregateCalls([
+          {
+            method: 'stakeAmount',
+            params: [validator]
+          }
+        ])
+        return stakeAmountResult
+      }
+    })
   }
 
   @logPerformance('ConsensusService::GetValidatorData')
@@ -568,23 +567,21 @@ export class ConsensusService {
   }
 
   private async getNodeByAddress (nodeAddress: string) {
-    const cacheKey = `nodeData-${nodeAddress}`
-    let nodeData = await this.cacheManager.get<Record<string, any>>(cacheKey)
-    if (isEmpty(nodeData)) {
-      const url = `${this.botApi}/node=${nodeAddress}`
-      const { Node } = await this.httpProxyGet(url)
-      nodeData = { ...Node }
-      await this.cacheManager.set(cacheKey, nodeData, this.cacheDuration('low'))
-    }
-
-    return nodeData
+    return this.getCacheOrFetch({
+      cacheKey: `nodeData-${nodeAddress}`,
+      volatility: 'low',
+      fetchFunction: async () => {
+        const url = `${this.botApi}/node=${nodeAddress}`
+        const { Node } = await this.httpProxyGet(url)
+        return Node
+      }
+    })
   }
 
   @logPerformance('ConsensusService::GetTotalSupply')
   async getTotalSupply () {
     const url = `${this.botApi}/stats/total_supply_simple`
-    const responseData = await this.httpProxyGet(url)
-    return responseData
+    return this.httpProxyGet(url)
   }
 
   private async httpProxyGet (url: string) {
@@ -595,22 +592,16 @@ export class ConsensusService {
     while (retryCount < maxRetries) {
       try {
         const responseData = await lastValueFrom(
-          this.httpService.get(url)
-            .pipe(
-              map((response) => {
-                return response.data
-              })
-            )
-            .pipe(
-              catchError(e => {
-                throw new HttpException(
-                  `${e?.response?.statusText}: ${e?.response?.data?.error}`,
-                  e?.response?.status
-                )
-              })
-            )
+          this.httpService.get(url).pipe(
+            map(response => response.data),
+            catchError(e => {
+              throw new HttpException(
+                `${e?.response?.statusText}: ${e?.response?.data?.error}`,
+                e?.response?.status
+              )
+            })
+          )
         )
-
         return responseData
       } catch (error) {
         if (retryCount === maxRetries - 1) throw error // Throw error on last retry
@@ -621,39 +612,62 @@ export class ConsensusService {
     }
   }
 
-  private cacheDuration (dataVolatility) {
-    switch (dataVolatility) {
-      case 'high': return 1000 * 60 * 5 // 5 minutes in milliseconds
-      case 'medium': return 1000 * 60 * 30 // 30 minutes in milliseconds
-      case 'low': return 1000 * 60 * 60 * 24 // 24 hours in milliseconds
+  private cacheDuration (volatility: 'high' | 'medium' | 'low'): number {
+    switch (volatility) {
+      case 'high':
+        return 1000 * 60 * 5 // 5 minutes
+      case 'medium':
+        return 1000 * 60 * 30 // 30 minutes
+      case 'low':
+        return 1000 * 60 * 60 * 24 // 24 hours
+      default:
+        return 1000 * 60 * 30 // Default to 30 minutes
     }
   }
 
-  private encodeFunctionData (method: string, params: any[]) {
+  private encodeFunctionData (method: string, params: any[]): string {
     return this.consensusInterface.encodeFunctionData(method, params)
   }
 
-  private createTransactionObject (encodedABI: string) {
+  private createTransactionObject (method: string, params: any[]) {
     return {
       to: this.consensusAddress,
-      data: encodedABI
+      data: this.encodeFunctionData(method, params)
     }
   }
 
   @logPerformance('ConsensusService::FetchValidatorsMap')
   private async fetchValidatorsMap () {
-    const cacheKey = 'validatorsMap'
-    let validatorsMap = await this.cacheManager.get(cacheKey)
-    if (isEmpty(validatorsMap)) {
-      const url = 'https://raw.githubusercontent.com/fuseio/console-dapp/master/validators/validators.json'
-      const responseData = await this.httpProxyGet(url)
-      validatorsMap = Object.entries(responseData).reduce((acc, [key, value]) => {
-        acc[key.toLowerCase()] = value
-        return acc
-      }, {})
-      await this.cacheManager.set(cacheKey, validatorsMap, this.cacheDuration('low'))
+    return this.getCacheOrFetch({
+      cacheKey: 'validatorsMap',
+      volatility: 'low',
+      fetchFunction: async () => {
+        const url = 'https://raw.githubusercontent.com/fuseio/console-dapp/master/validators/validators.json'
+        const responseData = await this.httpProxyGet(url)
+        return Object.entries(responseData).reduce((acc, [key, value]) => {
+          acc[key.toLowerCase()] = value
+          return acc
+        }, {})
+      }
+    })
+  }
+
+  private async getCacheOrFetch<T> ({
+    cacheKey,
+    volatility,
+    fetchFunction,
+    forceRefresh = false,
+    refreshCondition
+  }: CacheOptions<T>): Promise<T> {
+    let cachedValue = await this.cacheManager.get<T>(cacheKey)
+
+    const shouldRefresh = forceRefresh || (refreshCondition && refreshCondition(cachedValue))
+
+    if (isEmpty(cachedValue) || shouldRefresh) {
+      cachedValue = await fetchFunction()
+      await this.cacheManager.set(cacheKey, cachedValue, this.cacheDuration(volatility))
     }
 
-    return validatorsMap
+    return cachedValue
   }
 }

d4c57aa1f716e24ac645ea667b59435d4a1278e8
Samet Sahin <a.sahin.ual@gmail.com>
2025-02-13 00:55:21 +0300

    add Masterchef V3 subgraph URL, change token addresses on stakingOptionsV2

--- apps/charge-network-service/src/common/config/configuration.ts
+++ apps/charge-network-service/src/common/config/configuration.ts
@@ -11,8 +11,8 @@ export default () => ({
   masterChefV3Address: '0xE3e184a7b75D0Ae6E17B58F5283b91B4E0A2604F',
   usdcOnStargateAddress: '0xc6Bc407706B7140EE8Eef2f86F9504651b63e7f9',
   wethOnStargateAddress: '0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590',
-  usdcOnStargatePoolId: 31,
-  wethOnStargatePoolId: 32,
+  usdcOnStargatePoolId: '31',
+  wethOnStargatePoolId: '32',
 
   validatorFee: '0.15',
   rpcConfig: {
@@ -30,6 +30,7 @@ export default () => ({
   voltageV2GraphUrl: 'https://gateway-arbitrum.network.thegraph.com/api/550967d6d70d7fce0a710f38dc7bc5df/subgraphs/id/B4BGk9itvmRXzzNRAzBWwQARHRt3ZvLz11aWNVsZPT4',
   liquidStakingFuseGraphUrl: 'https://gateway-arbitrum.network.thegraph.com/api/3f81974147b5b63470524ed08206e24e/subgraphs/id/7FQVAoYfsrYPAVzaHnky1rHGYjXj2hcw3yokeLQmpntp',
   voltageV3GraphUrl: 'https://gateway-arbitrum.network.thegraph.com/api/550967d6d70d7fce0a710f38dc7bc5df/subgraphs/id/HzpnoLiTRga8yWaPBPBJjLp1FseiJkiynKDNXXFDKEQc',
+  masterChefV3GraphUrl: 'https://gateway.thegraph.com/api/550967d6d70d7fce0a710f38dc7bc5df/subgraphs/id/4DwVLaAaEuutpoCwmGUNBS45mSnGABt42u1Qbf73BqbR',
   unmarshal: {
     baseUrl: process.env.UNMARSHAL_BASE_URL || 'https://api.unmarshal.com',
     apiKey: process.env.UNMARSHAL_AUTH_KEY
@@ -61,22 +62,22 @@ export default () => ({
   ],
   stakingOptionsV2: [
     {
-      tokenAddress: '0x620fd5fa44BE6af63715Ef4E65DDFA0387aD13F5',
+      tokenAddress: '0xc6Bc407706B7140EE8Eef2f86F9504651b63e7f9',
       tokenSymbol: 'USDC',
       tokenName: 'USD Coin',
       tokenLogoURI:
         'https://raw.githubusercontent.com/voltfinance/token-logos/main/logos/0x620fd5fa44BE6af63715Ef4E65DDFA0387aD13F5/logo.png',
-      unStakeTokenAddress: '0x97a6e78c9208c21afaDa67e7E61d7ad27688eFd1',
+      unStakeTokenAddress: '0xc6Bc407706B7140EE8Eef2f86F9504651b63e7f9',
       stakingProviderId: 'usdc-on-stargate-simple-staking',
       expired: false
     },
     {
-      tokenAddress: '0xa722c13135930332Eb3d749B2F0906559D2C5b99',
+      tokenAddress: '0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590',
       tokenSymbol: 'WETH',
       tokenName: 'Wrapped Ether',
       tokenLogoURI:
         'https://raw.githubusercontent.com/voltfinance/token-logos/main/logos/0xa722c13135930332Eb3d749B2F0906559D2C5b99/logo.png',
-      unStakeTokenAddress: '0x97a6e78c9208c21afaDa67e7E61d7ad27688eFd1',
+      unStakeTokenAddress: '0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590',
       stakingProviderId: 'weth-on-stargate-simple-staking',
       expired: false
     }
